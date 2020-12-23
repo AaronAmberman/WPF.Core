6549dff99b11da82dc032076b48efe366b72a205
Aaron Amberman <aaronamberman@gmail.com>
2020-12-23 07:45:09 -0500

    Added extension methods and extended observable collection

---
+++ Wpf.Core/Wpf.Core/ExtendedObservableCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Wpf.Core
+{
+    public class ExtendedObservableCollection<T> : ObservableCollection<T>
+    {
+        public void AddRange(IEnumerable<T> items)
+        {
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            if (!items.Any()) return;
+
+            CheckReentrancy();
+
+            foreach(T item in items)
+            {
+                Items.Add(item);
+            }
+
+            OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+            OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, items.ToList()));
+        }
+
+        public void RemoveRange(IEnumerable<T> items)
+        {
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            if (!items.Any()) return;
+
+            CheckReentrancy();
+
+            foreach (T item in items)
+            {
+                Items.Remove(item);
+            }
+
+            OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+            OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, items.ToList()));
+        }
+    }
+}
---
+++ Wpf.Core/Wpf.Core/ExtensionMethods.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Wpf.Core
+{
+    public static class ExtensionMethods
+    {
+        #region ICollection<T>
+
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> enumerable)
+        {
+            foreach (T t in enumerable)
+            {
+                collection.Add(t);
+            }
+        }
+
+        public static void RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> enumerable)
+        {
+            foreach (T t in enumerable)
+            {
+                collection.Remove(t);
+            }
+        }
+
+        #endregion
+
+        #region IList
+
+        public static void AddRange<T>(this IList list, IEnumerable<T> enumerable)
+        {
+            foreach (T t in enumerable)
+            {
+                list.Add(t);
+            }
+        }
+
+        public static void RemoveRange<T>(this IList list, IEnumerable<T> enumerable)
+        {
+            foreach (T t in enumerable)
+            {
+                list.Remove(t);
+            }
+        }
+
+        #endregion
+    }
+}
